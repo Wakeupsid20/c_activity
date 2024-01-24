3fe123fbb711b2bc613634f90d1e113ec56d6ee5
wakeupsid20 <siddhip.bsc23@rvu.edu.in>
2024-01-24 10:32:08 +0530

    test

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 int input();
 int compare(int a,int b, int c);
-void output(int)
+void output(int a, int b,int c,int largest);
 
-int input();
-int compare(int a, int b, int c);
-void output(int a, int b, int c, int largest);
+int main()
+{
+    int num1,num2,num3,largest;
 
-int main() {
-    int num1, num2, num3, largest;
+       num1 = input();
+       num2 = input();
+       num3 = input();
 
- 
-    num1 = input();
-    num2 = input();
-    num3 = input();
+
+
 
    
     largest = compare(num1, num2, num3);
