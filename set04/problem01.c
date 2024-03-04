e9c163f66d33fabe9fdfde250c1bc62e0a410dec
wakeupsid20 <siddhip.bsc23@rvu.edu.in>
2024-03-04 14:44:49 +0530

    test

--- set04/problem01.c
+++ set04/problem01.c
@@ -1,1 +1,14 @@
 
+Write a program to find sum of two fractions
+Function Declarations
+
+void input(int *num1, int *den1, int *num2, int *den2);
+void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den);
+void output(int num1, int den1, int num2, int den2, int res_num, int res_den);
+Input
+
+1 4
+1 2
+Output
+
+1/2 + 1/4 = 3/4
